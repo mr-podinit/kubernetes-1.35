71213bd66d3288ad811835b5f792402e77526415
angeloxx <angeloxx@angeloxx.it>
2025-12-27 08:12:37 +0100

    kind configuration

---
+++ kind/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo kind delete cluster --name k8s-1.35 || true
+sudo kind create cluster --name k8s-1.35 --config config.yaml
+sudo kind get kubeconfig --name k8s-1.35 | tee ~/.kube/config >/dev/null
